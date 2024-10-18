307d9bf46027e87365b705b9a5bc6404715d7f57
OmarWatany <watany6500@gmail.com>
2024-10-18 22:34:37 +0300

    reorganizing code

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 import plotly.graph_objects as go
 
 
-def Plot_signal_cont(fig_cont, Title: str, Color: str, Signal, Indices):
+def scatter_cont_sig(fig_cont, Title: str, Color: str, Signal, Indices):
     # Add a continuous line for the signal
     ln = 50 if len(Indices) > 50 else len(Indices)
     fig_cont.add_trace(
@@ -26,7 +26,7 @@ def Plot_signal_cont(fig_cont, Title: str, Color: str, Signal, Indices):
     )
 
 
-def Plot_signal_stem(fig_disc, Title: str, Color: str, Signal, Indices):
+def scatter_disc_sig(fig_disc, Title: str, Color: str, Signal, Indices):
     # Add a continuous line for the signal
     ln = 50 if len(Indices) > 50 else len(Indices)
     for i in range(ln):
@@ -64,97 +64,94 @@ def Plot_signal_pd(Continuous: bool, Signal, Indices):
     if Continuous:
         st.line_chart(dt)
 
-# RUN START FROM HERE
 
-st.write("""
-# DSP Framework
-""")
-
-operation = st.selectbox(
-    "Choose Operation",
-    ("Read from file", "Generate"),
-)
-
-if operation == "Generate":
-    sin_flag = st.selectbox(
-        "Chose wave",
-        ("Sin", "Cos", "both"),
+def generated_signal_args() -> sp.Signal:
+    sin_flag = (
+        st.selectbox(
+            "Chose wave",
+            ("Sin", "Cos"),
+        )
+        == "Sin"
     )
-    amp = st.number_input("Insert Amp", value=0)
-    phase_shift = st.number_input("Insert Phase shift",format="%f")
-    freq = st.number_input("Insert Freq", value=0)
-    samplingFreq = st.number_input("Insert Sampling Freq", value=0)
+    amp = st.number_input("Insert Amp", value=1)
+    phase_shift = st.number_input("Insert Phase shift", value=0.0, format="%f")
+    freq = st.number_input("Insert Freq", value=1)
+    samplingFreq = st.number_input("Insert Sampling Freq", value=10)
+
     # TODO: Handle Error
     # if samplingFreq < 2 * freq:
 
     clicked = st.button("Show", type="primary")
-
-    fig_width = 10
-    fig_height = fig_width / 2
-
     if clicked:
+        sig = sp.generate_signal(
+            sin_flag,
+            False,
+            sp.Signal_type.Time,
+            amp,
+            samplingFreq,
+            freq,
+            phase_shift,
+        )
+        return sig
+    return sp.Signal()
+
+
+def Plot_signal(s, indices):
+    cont_fig = go.Figure()
+    disc_fig = go.Figure()
+    scatter_cont_sig(cont_fig, "Signal", "blue", s, indices)
+    scatter_disc_sig(disc_fig, "Signal", "blue", s, indices)
+    st.plotly_chart(disc_fig)
+    st.plotly_chart(cont_fig)
+
+
+def add_sig(signal_1, signal_2):
+    pass
+
+
+def sub_sig(signal_1, signal_2):
+    pass
+
+
+if __name__ == "__main__":
+    st.write("""
+    # DSP Framework
+    """)
+
+    operation = st.selectbox(
+        "Choose Operation",
+        ("Read from file", "Generate"),
+    )
+    if operation == "Generate":
+        sig = generated_signal_args()
+        Plot_signal(sig.amplitudes, sig.indices)
+    else:
+        uploaded_file = st.file_uploader("Upload a signal txt file", type="txt")
+        read_button = st.button("Read Signal")
+        if read_button and uploaded_file is not None:
+            sig = sp.read_file(uploaded_file)
+            Plot_signal(sig.amplitudes, sig.indices)
+
+    # Temporary
+    temp_clicked = st.button("Show Temp", type="primary", disabled=True)
+    if temp_clicked:
+        s, s_indices = sp.generate_signal(
+            True, False, sp.Signal_type.Time, 3, 720, 360, 1.96349540849362
+        )
+
+        c, c_indices = sp.generate_signal(
+            False, False, sp.Signal_type.Time, 3, 500, 200, 2.35619449019235
+        )
+
+        # showing sin and cosine signals at the same time
         cont_fig = go.Figure()
         disc_fig = go.Figure()
 
-        if sin_flag == "Sin":
-            s, indices = sp.generate_signal(
-            True, False, sp.Signal_type.Time, amp, samplingFreq, freq, phase_shift
-            )
-            Plot_signal_cont(cont_fig,"Sine Wave","blue", s, indices)
-            Plot_signal_stem(disc_fig,"Sine Wave","blue ", s, indices)
-        elif sin_flag == "Cos":
-            s, indices = sp.generate_signal(
-                False, False, sp.Signal_type.Time, amp, samplingFreq, freq, phase_shift
-            )
-            Plot_signal_cont(cont_fig,"Cosine Wave","blue", s, indices)
-            Plot_signal_stem(disc_fig,"Cosine Wave","blue", s, indices)
-        elif sin_flag == "both":
-            s, s_indices = sp.generate_signal(
-                True, False, sp.Signal_type.Time, amp, samplingFreq, freq, phase_shift
-            )
-            c, c_indices = sp.generate_signal(
-                False, False, sp.Signal_type.Time, amp, samplingFreq, freq, phase_shift
-            )
-            Plot_signal_cont(cont_fig,"Sine Wave","blue", s, s_indices)
-            Plot_signal_stem(disc_fig,"Sine Wave","blue", s, s_indices)
-            Plot_signal_cont(cont_fig,"Cosine Wave","red", c, c_indices)
-            Plot_signal_stem(disc_fig,"Cosine Wave","red", c, c_indices)
+        scatter_cont_sig(cont_fig, "Sin Wave", "blue", s, s_indices)
+        scatter_disc_sig(disc_fig, "Sin Wave", "blue", s, s_indices)
+
+        scatter_cont_sig(cont_fig, "Cos Wave", "red", c, s_indices)
+        scatter_disc_sig(disc_fig, "Cos Wave", "red", c, c_indices)
+
         st.plotly_chart(disc_fig)
         st.plotly_chart(cont_fig)
-
-else:
-    uploaded_file = st.file_uploader("Upload a signal txt file", type="txt")
-    read_button = st.button("Read Signal")
-    if read_button:
-        if uploaded_file is not None:
-            s,indices  = sp.read_file(uploaded_file)
-            cont_fig = go.Figure()
-            disc_fig = go.Figure()
-
-            Plot_signal_cont(cont_fig, "file", "blue", s,indices)
-            Plot_signal_stem(disc_fig, "file", "blue", s, indices)
-            st.plotly_chart(cont_fig)
-            st.plotly_chart(disc_fig)
-
-
-# Temporary
-s, s_indices = sp.generate_signal(
-    True, False, sp.Signal_type.Time, 3, 720, 360, 1.96349540849362
-)
-
-c, c_indices = sp.generate_signal(
-    False, False, sp.Signal_type.Time, 3, 500, 200, 2.35619449019235
-)
-
-# showing sin and cosine signals at the same time
-cont_fig = go.Figure()
-disc_fig = go.Figure()
-
-Plot_signal_cont(cont_fig, "Sin Wave", "blue", s, s_indices)
-Plot_signal_stem(disc_fig, "Sin Wave", "blue", s, s_indices)
-
-Plot_signal_cont(cont_fig, "Cos Wave", "red", c, s_indices)
-Plot_signal_stem(disc_fig, "Cos Wave", "red", c, c_indices)
-
-st.plotly_chart(disc_fig)
-st.plotly_chart(cont_fig)
--- signalProcessing.py
+++ signalProcessing.py
@@ -7,6 +7,25 @@ class Signal_type(Enum):
     Freq = 1
 
 
+class Signal:
+    periodic: bool
+    signal_type: Signal_type
+    amplitudes: []
+    indices: []
+
+    def __init__(
+        self,
+        periodic: bool = False,
+        signal_type: Signal_type = Signal_type.Time,
+        amps=[],
+        indices=[],
+    ):
+        self.periodic = periodic
+        self.signal_type = signal_type
+        self.amplitudes = amps
+        self.indices = indices
+
+
 def generate_signal(
     sin_flag: bool,
     periodic: bool,
@@ -15,26 +34,36 @@ def generate_signal(
     sampling_freq,
     analog_freq,
     phase_shift,
-):
+) -> Signal:
     # TODO: Handle Division by 0
     w = 2 * math.pi * (analog_freq / sampling_freq)
     if sin_flag:
         s = [amp * math.sin(w * i + phase_shift) for i in range(0, sampling_freq)]
     else:
         s = [amp * math.cos(w * i + phase_shift) for i in range(0, sampling_freq)]
-    return s, [i for i in range(0, sampling_freq)]
+    return Signal(periodic, signal_type, s, [i for i in range(0, sampling_freq)])
+    # return s, [i for i in range(0, sampling_freq)]
 
-def read_file(uploaded_file):
+
+def read_file(uploaded_file) -> Signal:
     file_content = uploaded_file.read().decode("utf-8").splitlines()
 
-    timeFlag = file_content[0]  # First line
-    periodicFlag = file_content[1]  # Second line
+    timeFlag = bool(file_content[0])  # First line
+    periodic = bool(file_content[1])  # Second line
     nOfSamples = int(file_content[2])  # Third line
 
     indices = []
     amplitudes = []
-    for line in file_content[3: 3 + nOfSamples]:
+    for line in file_content[3 : 3 + nOfSamples]:
         values = line.strip().split(" ")
         indices.append(int(values[0]))
         amplitudes.append(float(values[1]))
-    return amplitudes, indices
+
+    return Signal(
+        periodic,
+        Signal_type.Time if timeFlag else Signal_type.Freq,
+        amplitudes,
+        indices,
+    )
+    # return amplitudes, indices
+
